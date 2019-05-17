2b396e1012eb186cfb85330725f99abd10179190
defaultuser1000 <and.zak.one@gmail.com>
2019-05-18 01:36:41 +0700

    changed configure

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -34,9 +34,9 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     services.register(middlewares)
     config.prefer(MemoryKeyedCache.self, for: KeyedCache.self)
     
-    let config = PostgreSQLDatabaseConfig(hostname: "localhost", port: 5432, username: "azakrzhevskiy", database: "lighthouse", password: nil, transport: .cleartext)
-//    let config = PostgreSQLDatabaseConfig(url: "postgres://jksospvxgfvmqy:1ab1b512ef684bd976bd29b6d0a49daac97b30c6bb89775d92e4a503e802e58e@ec2-54-246-92-116.eu-west-1.compute.amazonaws.com:5432/dak5od9bde62sg")
-    let postgres = PostgreSQLDatabase(config: config)
+//    let config = PostgreSQLDatabaseConfig(hostname: "localhost", port: 5432, username: "azakrzhevskiy", database: "lighthouse", password: nil, transport: .cleartext)
+    let config = PostgreSQLDatabaseConfig(url: "postgres://jksospvxgfvmqy:1ab1b512ef684bd976bd29b6d0a49daac97b30c6bb89775d92e4a503e802e58e@ec2-54-246-92-116.eu-west-1.compute.amazonaws.com:5432/dak5od9bde62sg")
+    let postgres = PostgreSQLDatabase(config: config!)
     
     var databases = DatabasesConfig()
     databases.add(database: postgres, as: .psql)
