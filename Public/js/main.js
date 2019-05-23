0cc9c40b2b414e0f947c8730711af1d5d5b84eff
defaultuser1000 <and.zak.one@gmail.com>
2019-05-24 02:45:40 +0700

    orders in table

--- Public/js/main.js
+++ Public/js/main.js
@@ -11,3 +11,34 @@ function sideNavAction() {
         //document.getElementById("middle-header").style.marginLeft = "260px";
     }
 }
+
+window.addEventListener('DOMContentLoaded', function(e) {
+
+    let dateFields = document.getElementsByClassName('dateCreated');
+    for (let i = 0; i < dateFields.length; i++) {
+        const cellDate = dateFields.item(i).textContent;
+        let date = new Date(1970, 0, 1);
+        date.setSeconds(cellDate);
+        const day = date.getDate();
+        let month = "";
+        if ((date.getMonth() + 1) < 10) {
+            month = "0" + (date.getMonth() + 1);
+        } else {
+            month = date.getMonth() + 1;
+        }
+        const year = date.getFullYear();
+        const hours = date.getHours();
+        const minutes = date.getMinutes();
+        dateFields.item(i).textContent = day + "." + month + "." + year + ", " + hours + ":" + minutes;
+    }
+} );
+
+$(document).ready(function() {
+
+    $('#orders-table tbody tr').click(function() {
+        let href = $(this).find("a").attr("href");
+        if(href) {
+            window.location = href;
+        }
+    });
+});
